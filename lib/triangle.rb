765c52f5bdf60be24b21d3e24f077942bb0a68a6
Michael Chung <mikechung89@gmail.com>
2022-04-06 09:49:10 -0400

    commit tree 419f62230dc99462c55b0715f4cde3ab400d095b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a triangle"
+    end
+  end
+
+  def kind
+    triangle = [@side1, @side2, @side3]
+    if triangle.uniq.length == 1
+      :equilateral
+    elsif triangle.uniq.length == 2
+      :isosceles
+    elsif triangle.uniq.length == 3
+      :scalene
+    elsif triangle.length != 3
+      begin
+      raise TriangleError
+      puts TriangleError.message
+      end
+    end
+  end
 end
